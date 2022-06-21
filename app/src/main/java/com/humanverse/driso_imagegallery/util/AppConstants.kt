7c93d66d1e938e53ac212300c4d44041d3d93ee5
Fakhrul Alam Siddiqei <50808070+FakhrulASA@users.noreply.github.com>
2022-06-21 23:57:43 +0600

    Update AppConstants.kt

--- app/src/main/java/com/humanverse/driso_imagegallery/util/AppConstants.kt
+++ app/src/main/java/com/humanverse/driso_imagegallery/util/AppConstants.kt
@@ -2,7 +2,7 @@ package com.humanverse.driso_imagegallery.util
 
 object AppConstants {
     fun getBaseUrl(): String = "https://api.unsplash.com/"
-    fun getClientKey(): String = "Client-ID wAkR5LSZf7uv7Fej1jLb2PLkyKBb9UzycDqZGTAK4SU"
+    fun getClientKey(): String = "Client-ID YOUR_TOKEN" // GET_UR_PERSONAL_CLIENT_ID_AND_REPLACE WITH YOUR_TOKEN PART ONLY FROM UNPLASH DEVELOPERS
     const val UNSPLASH_API = 1
     const val PICASSO_API = 2
 }
